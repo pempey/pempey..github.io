900f754284233e77be089a0f50a46b0b474717a1
pempey <empey.peter@gmail.com>
2015-09-05 18:53:54 -0700

    Began adding a portfolio
    Added the beginning for a resume page and a portfolio.  Also added a
    page for the VCR class.

---
+++ js/portflo.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+$("#vis-a-1").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-1').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
+$("#vis-a-2").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-2').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
+$("#vis-a-3").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-3').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
+$("#vis-a-4").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-4').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
+$("#vis-a-5").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-5').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
