30a6808147f1ea8805b32a29580dd7b1e0e0dcd3
pempey <empey.peter@gmail.com>
2015-09-12 14:38:05 -0700

    Week 2
    Added the images from the week 2 assignment.

--- js/portflo.js
+++ js/portflo.js
@@ -24,3 +24,38 @@ $("#vis-a-5").on("click", function() {
    $('#imagemodal').modal('show');
    
 });
+$("#vis-a-6").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-6').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
+$("#vis-a-7").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-7').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
+$("#vis-a-8").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-8').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
+$("#vis-a-9").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-9').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
+$("#vis-a-10").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-10').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
+$("#vis-a-11").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-11').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
+$("#vis-a-12").on("click", function() {
+   $('#imagepreview').attr('src', $('#vis-img-12').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
